aa13dbfe0aeea671dd26fb265d536ad925aceae2
brye <bryewalks@gmail.com>
2020-05-12 15:00:15 -0500

    initial commit

---
+++ src/Components/Globals/index.js
@@ -0,0 +1,9 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    background-color: lightgray;
+  }
+`
