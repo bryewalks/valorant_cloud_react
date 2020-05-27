ed53752e11f01e4b7340e4848dc55b7648e9a617
brye <bryewalks@gmail.com>
2020-05-27 16:45:28 -0500

    changed marker list to table

--- src/Components/MarkerInfo/style.js
+++ src/Components/MarkerInfo/style.js
@@ -7,12 +7,12 @@ export const InfoWrapper = styled.div`
 
 export const InfoContainer = styled.div`
   width: 0px;
-  height: 90vh;
+  height: 75vh;
   transform: translate(-750px);
   background-color: white;
   transition: transform 1s ease-out;
   ${props => props.expanded && css`
-    width: 750px;
+    width: 25vw;
     transform: translate(0px);
   `}
 `
--- src/Components/MarkerList/style.js
+++ src/Components/MarkerList/style.js
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components'
 
 export const MarkersContainer = styled.div`
-  background-color: white;
+  background-color: #707070;
   margin: 20px auto;
   height: 500px;
   width: 1000px;
@@ -15,5 +15,28 @@ export const MarkerListItem = styled.div`
   `}
 `
 
-export const RemoveButton = styled.button`
+export const MarkersTable = styled.table`
+  text-align: center;
+  border-collapse: collapse;
+  tr {
+    height: 40px;
+    background-color: #707070;
+  }
+  tr:nth-child(odd) {
+    background-color: #505050;
+  }
+  th {
+    width: 100%;
+    background-color: #252627;
+    color: white;
+    :first-child {
+      width: 50px;
+    }
+  }
+`
+
+export const MarkerRow = styled.tr`
+  ${props => props.selected && css`
+    color: white;
+  `}
 `
