181ffd99e0477f5650bb95d0808f5c0966460abf
brye <bryewalks@gmail.com>
2020-05-28 20:28:43 -0500

    fixed missing thead and tbody

--- src/Components/MarkerList/index.js
+++ src/Components/MarkerList/index.js
@@ -6,21 +6,25 @@ export const MarkerList = ({ markers, markerClick, selectedMarker, removeMarker 
   return (
     <MarkersContainer>
       <MarkersTable>
-        <tr>
-          <th>Type</th>
-          <th>Title</th>
-          <th>Delete</th>
-        </tr>
-      { markers.map((marker, index) =>
-        <MarkerRow key={ index } 
-            onClick={ () => markerClick(marker) } 
-            selected={ marker.id === selectedMarker.id }>
-          <td>{ marker.location_type }</td>
-          <td>{ marker.title }</td>
-          <td onClick={ (event) => {event.stopPropagation()
-                                    removeMarker(marker)}}>x</td>
-        </MarkerRow>
-      )}
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Title</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          { markers.map((marker, index) =>
+            <MarkerRow key={ index } 
+                onClick={ () => markerClick(marker) } 
+                selected={ marker.id === selectedMarker.id }>
+              <td>{ marker.location_type }</td>
+              <td>{ marker.title }</td>
+              <td onClick={ (event) => {event.stopPropagation()
+                                        removeMarker(marker)}}>x</td>
+            </MarkerRow>
+          )}
+        </tbody>
       </MarkersTable>
     </MarkersContainer>
   )
